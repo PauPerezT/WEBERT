5b97840084ade1fbee71c57e205de3c5ebc44ce0
P. A. Perez-Toro <36645902+PauPerezT@users.noreply.github.com>
2021-02-19 15:53:58 +0100

    Update WEBERT.py
    correction typo

--- WEBERT.py
+++ WEBERT.py
@@ -121,7 +121,7 @@ class BERT:
             
             
             if self.stopwords:
-                text=StopWordsRemoval(text_aux,self.Language)
+                text=StopWordsRemoval(text_aux,self.language)
             self.words.append(text_org.split())
             docs.append(text_aux)
         return docs
